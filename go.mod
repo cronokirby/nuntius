b1e003c14c2d96d0c93d63041ad57bd0b6061d32
Lúcás Meier <cronokirby@gmail.com>
2021-05-24 20:38:27 +0200

    Setup client database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cronokirby/nuntius
 
 go 1.16
+
+require modernc.org/sqlite v1.10.7
--- main.go
+++ main.go
@@ -2,12 +2,51 @@ package main
 
 import (
 	"crypto/ed25519"
+	"database/sql"
+
 	"encoding/hex"
 	"fmt"
 	"os"
+
+	_ "modernc.org/sqlite"
 )
 
+const CLIENT_DATABASE = "./client.db"
+
+type clientDatabase struct {
+	db *sql.DB
+}
+
+func newClientDatabase() (*clientDatabase, error) {
+	db, err := sql.Open("sqlite", CLIENT_DATABASE)
+	if err != nil {
+		return nil, err
+	}
+	return &clientDatabase{db}, nil
+}
+
+func (db *clientDatabase) setup() error {
+	_, err := db.db.Exec(`
+	CREATE TABLE IF NOT EXISTS identity (
+		public BLOB PRIMARY KEY,
+  	private BLOB
+	);
+	`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func commandGenerateIdentity() error {
+	db, err := newClientDatabase()
+	if err != nil {
+		return fmt.Errorf("couldn't connect to database: %w", err)
+	}
+	err = db.setup()
+	if err != nil {
+		return err
+	}
 	pub, priv, err := ed25519.GenerateKey(nil)
 	if err != nil {
 		return fmt.Errorf("couldn't generate identity pair: %w", err)
