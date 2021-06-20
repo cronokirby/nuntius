f7f1124e1256fd4ac34d16d938f6caef9aa75c2a
Lúcás Meier <cronokirby@gmail.com>
2021-06-20 20:00:10 +0200

    Add chain key kdf

--- internal/crypto/ratchet.go
+++ internal/crypto/ratchet.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
 	"io"
 
@@ -28,7 +29,7 @@ var kdfRootKeyInfo = []byte("Nuntius Root Key KDF 2021-06-20")
 
 // kdfRootKey uses a root key, and a shared secret, to derive a new root key, and a chain key
 //
-// Corresponds to the KDF_RK function.
+// This corresponds to the KDF_RK function.
 func kdfRootKey(rk rootKey, dhOut exchangedSecret) (rootKey, chainKey, error) {
 	reader := hkdf.New(sha256.New, dhOut, rk, kdfRootKeyInfo)
 	rootKey := rootKey(make([]byte, rootKeySize))
@@ -42,4 +43,31 @@ func kdfRootKey(rk rootKey, dhOut exchangedSecret) (rootKey, chainKey, error) {
 		return nil, nil, err
 	}
 	return rootKey, chainKey, nil
+}
+
+// messageKey represents a key used for encrypting messages
+type MessageKey []byte
+
+// MessageKeySize is the number of bytes in a message key
+const MessageKeySize = 32
+
+// kdfChainKey uses a chain key to derive a new chain and message key.
+//
+// This corresponds to the KDF_CK function
+func kdfChainKey(ck chainKey) (chainKey, MessageKey, error) {
+	hash := hmac.New(sha256.New, ck)
+	_, err := hash.Write([]byte{0})
+	if err != nil {
+		return nil, nil, err
+	}
+	ck = chainKey(hash.Sum(nil))
+
+	hash.Reset()
+	_, err = hash.Write([]byte{1})
+	if err != nil {
+		return nil, nil, err
+	}
+	mk := MessageKey(hash.Sum(nil))
+
+	return ck, mk, nil
 }
--- internal/crypto/ratchet_test.go
+++ internal/crypto/ratchet_test.go
@@ -24,7 +24,25 @@ func TestKDFRootKey(t *testing.T) {
 	if bytes.Equal(newRk, rk) {
 		t.Errorf("derived root key matches root key: %v %v", newRk, rk)
 	}
-	if bytes.Equal(ck, rk) {
-		t.Errorf("chain key matches root key: %v %v", ck, rk)
+	if bytes.Equal(newRk, ck) {
+		t.Errorf("derived root key matches chain key: %v %v", newRk, ck)
+	}
+}
+
+func TestKDFChainKey(t *testing.T) {
+	ck := make([]byte, chainKeySize)
+	_, err := rand.Read(ck)
+	if err != nil {
+		t.Errorf("couldn't generate chain key: %v", err)
+	}
+	newCk, mk, err := kdfChainKey(ck)
+	if err != nil {
+		t.Errorf("couldn't derive root key: %v", err)
+	}
+	if bytes.Equal(newCk, ck) {
+		t.Errorf("derived chain key matches chain key: %v %v", newCk, ck)
+	}
+	if bytes.Equal(newCk, mk) {
+		t.Errorf("derived chain key matches message key: %v %v", newCk, mk)
 	}
 }
